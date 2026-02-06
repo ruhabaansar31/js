d74c2ff0af3416312016da8300f39bb8e19e89b8
ruhabaansar31 <ruhabaansar@gmail.com>
2026-02-07 03:54:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// let user1="Ali"
+// let user2="Ahmed"
+// alert("Hello" + user1 + "&" + user2 )
+
+// let user1="Ali"
+// let user2="Ahmed"
+// alert(`hello ${user1} & ${user2}`);
+
+
+// function add(a,b){
+//     return a ** b
+// }
+// alert (add(2,4))
+
+// function ruhaba(a,b){
+//     return a ** b;
+// }
+// let a= Number('prompt{"enter the 1st value" }');
+// let b= Number('prompt{"enter the 2nd value" }');
+//       alert (ruhaba(a,b));
+
+// let a = "Pakistan";
+// let b = "India";
+
+// alert(`hello ${a} & ${b}`);
+
+
+
+// for ( let i=1; i<=10; i++){
+//     console.log(`Ruba ${i}`);
+// }
+
+// let c= Number(prompt("enter the table value you want" ));
+// for (let i=1; i<=10 ; i++){
+//     console.log(`${c} x ${i} = ${c*i}`);
+// }
+
+
+
+// let i=1;
+// while (i<=10){
+//     console.log(`hello ${i}`);
+//     i++;
+// }
+
+// let c= Number(prompt("enter the table value you want" ));
+// let i=1;
+// while(i<=10){
+//     console.log(`${c} x ${i} = ${c*i}`);
+//     i++;
+// }
